7bcecc5c826ce5cb5e7ae7120539ae8a02440b0c
github username <harr.hughes@gmail.com>
2020-09-21 19:04:32 +0100

    q1 started

---
+++ question1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+class Period:
+
+    def __init__(self, start, end, points):
+        self.points = points
+
+    def point_count(self):
+        return len(self.points)
+
+
+if __name__ == "__main__":
+    pass
