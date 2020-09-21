8d98bec3c293a13b58187c5e092dee280af1d978
github username <harr.hughes@gmail.com>
2020-09-21 20:41:41 +0100

    q1 progress

--- question1.py
+++ question1.py
@@ -10,5 +10,22 @@ class Period:
         return len(self.points)
 
 
+def period_generator(list1, list2):
+    periods = []
+    # print(range(len(list1) - 2))
+    for i in range(len(list1) - 1):
+        [start, end] = [list1[i], list1[i+1]]
+        points = []
+        for i in range(start, end):
+
+        periods.append(Period(start, end, points))
+    return periods
+
+
 if __name__ == "__main__":
-    pass
+    list1 = [1, 5, 6, 10, 11, 20, 21, 25, 26, 40, 41, 50]
+    list2 = [14, 9, 24, 2, 44, 8, 41, 4, 46, 26,
+             11, 31, 18, 24, 21, 4, 22, 50, 6, 36]
+    periods = period_generator(list1, list2)
+    print(period[0].start, period[0].end,
+          period[0].points, period[0].point_count)
