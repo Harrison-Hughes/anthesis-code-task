c162215e05c7ecb3d293eed2f94b279a02f9fefe
github username <harr.hughes@gmail.com>
2020-09-22 23:26:50 +0100

    q3 complete

--- q3django/farms/models.py
+++ q3django/farms/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Farm(models.Model):
     name = models.CharField(max_length=50)
@@ -14,7 +12,3 @@ class Field(models.Model):
     farm = models.ForeignKey(Farm, on_delete=models.CASCADE)
     size_in_sq_km = models.FloatField()
     produce = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
--- q3django/farms/views.py
+++ q3django/farms/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Farm
+from .models import Farm, Field
 
 
-def index(req):
-    fields = Farm.objects.all()
-    return render(req, 'index.html', {'fields': fields})
+def show(req, farm_id):
+    farm = Farm.objects.get(pk=farm_id)
+    fields = Field.objects.filter(farm_id=farm_id)
+    return render(req, 'show.html', {'farm': farm, 'fields': fields})
