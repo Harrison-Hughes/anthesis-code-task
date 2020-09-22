8f9974d701044a99c3708308f6a6db917c810dda
github username <harr.hughes@gmail.com>
2020-09-23 00:59:37 +0100

    q4 done and q5 started

---
+++ q4sql.sql
@@ -0,0 +1,14 @@
+DELETE FROM orig_table 
+WHERE EXISTS (
+  SELECT 1
+FROM orig_table c
+WHERE c.user_name = orig_table.user_name AND c.site = orig_table.site AND c.customer = orig_table.customer AND c.supplier = orig_table.supplier AND c.rowid < orig_table.rowid
+);
+
+SELECT *
+FROM orig_table;
+
+-- this works by deleting all rows from the table for which there exists a duplicate row with a smaller row id
+-- I've made it compatible with sqlite3
+
+-- N.B I've chosen field name 'user_name' instead of 'user' simply to bypass linting errors
